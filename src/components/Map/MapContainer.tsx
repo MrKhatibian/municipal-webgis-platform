f2927e98526d911ae76a1826973a0b443854bc16
MrKhatibian <mr.khatibian98@gmail.com>
2026-09-22 14:45:21 +0330

    Map component

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,4 +1,31 @@
+import { useEffect, useRef } from 'react';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+
+
 export default function MapContainer() {
+    const mapDiv = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!mapDiv.current) {
+            return;
+        }
+        const map = new Map({
+            basemap: 'osm',
+        });
+
+        const view = new MapView({
+            container: mapDiv.current,
+            map,
+            center: [46.17, 37.39],
+            zoom: 13,
+        });
+
+        return () => {
+            view.destroy();
+        };
+    },[]);    
+
 	return (
         <div className="map-container">
             <div className="map-placeholder">
