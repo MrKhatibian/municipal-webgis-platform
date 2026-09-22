6c62f0c3c16d6713dd31270363464fadc54005c1
MrKhatibian <mr.khatibian98@gmail.com>
2026-09-22 12:54:57 +0330

    map component

---
+++ src/components/Map/MapContainer.tsx
@@ -0,0 +1,9 @@
+export default function MapContainer() {
+	return (
+        <div className="map-container">
+            <div className="map-placeholder">
+                Map
+            </div>
+        </div>
+	);
+}
