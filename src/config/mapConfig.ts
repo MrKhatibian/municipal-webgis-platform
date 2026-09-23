2f58e532bf9d5a5efdefa64cf21f358822c457b0
MrKhatibian <mr.khatibian98@gmail.com>
2026-09-23 09:57:08 +0330

    add featureServerUrl to map config

--- src/config/mapConfig.ts
+++ src/config/mapConfig.ts
@@ -3,5 +3,6 @@ export const mapConfig = {
     basemap: 'osm',
     center: [46.17, 37.39] as [number, number],
     zoom: 13,
-    spatialReference: 32638
+    spatialReference: 32638,
+    featureServerUrl: ""
 }
