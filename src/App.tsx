6a4709a53c4c2c05fa370968990ef0ebd4e3f242
MrKhatibian <mr.khatibian98@gmail.com>
2026-09-23 09:21:19 +0330

    Add state in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import MapContainer from './components/Map/MapContainer';
 import PropertyPanel from './components/Property/PropertyPanel';
 
 function App() {
+    const [layerVisibility, setLayerVisibility] = useState({
+        melk: true,
+        ayan: true,
+        tarh: true
+    });
+
     return (
         <div className="app">
             <Header />
