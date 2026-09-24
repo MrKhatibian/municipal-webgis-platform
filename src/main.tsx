62f0526c508b3a74fdcede92f500b038d1bc16bf
MrKhatibian <mr.khatibian98@gmail.com>
2026-09-24 10:21:03 +0330

    add font Farsi

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import '@arcgis/core/assets/esri/themes/light/main.css';
 import '@arcgis/map-components/components/arcgis-layer-list';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import '@fontsource-variable/vazirmatn';
+import '@fontsource-variable/vazirmatn/wght.css';
 import './index.css';
 import App from './App.tsx';
 
